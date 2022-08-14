d80d68ab8e065c11e1e200d33d490c53c31e2fe4
ramita-0 <ramiromoya321@gmail.com>
2022-08-14 20:53:12 -0300

    entra a js y clickea el ej de tabla

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const {Builder} = require('selenium-webdriver');
+const {Builder, WebDriver, By, Key, until, Actions} = require('selenium-webdriver');
+
+callbackAfterTimeout = (driver, timeInSeconds, callback) => {
+    driver.sleep(timeInSeconds * 1000).then(slept => {
+        callback()
+    })
+}
 
 (async function helloSelenium() {
     let driver = await new Builder().forBrowser('firefox').build();
 
     await driver.get('https://ramita-0.github.io/');
+    
+    // accediendo a la seccion javascript del portafolio
+    await driver.manage().setTimeouts({implicit: 3000})
+    await driver.findElement({linkText: "JAVASCRIPT"}).click()
 
+    // accediendo al ejemplo de tabla
+    await driver.findElement({className: "carousel-control-prev-icon"}).click()
+
+    callbackAfterTimeout(driver, 1, async () => {
+        let el = await driver.findElement({xpath: "/html/body/div[2]/div/div[3]/div/div[2]/div[3]/a"})
+        await driver.wait(until.elementIsVisible(el), 500);
+        await el.click();
+    })
 })();
