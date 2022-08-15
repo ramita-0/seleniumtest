71f0729f0cd859aabb2878fd334b247a98930d17
ramita-0 <ramiromoya321@gmail.com>
2022-08-14 21:46:35 -0300

    avances

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 const {Builder, WebDriver, By, Key, until, Actions} = require('selenium-webdriver');
 
+
 callbackAfterTimeout = (driver, timeInSeconds, callback) => {
     driver.sleep(timeInSeconds * 1000).then(slept => {
         callback()
     })
 }
 
-(async function helloSelenium() {
-    let driver = await new Builder().forBrowser('firefox').build();
-
-    await driver.get('https://ramita-0.github.io/');
-    
-    // accediendo a la seccion javascript del portafolio
-    await driver.manage().setTimeouts({implicit: 3000})
-    await driver.findElement({linkText: "JAVASCRIPT"}).click()
-
-    // accediendo al ejemplo de tabla
-    await driver.findElement({className: "carousel-control-prev-icon"}).click()
-
-    callbackAfterTimeout(driver, 1, async () => {
-        let el = await driver.findElement({xpath: "/html/body/div[2]/div/div[3]/div/div[2]/div[3]/a"})
-        await driver.wait(until.elementIsVisible(el), 500);
-        await el.click();
+timeout = time => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res()
+        }, time)
     })
+}
+
+(async function main() {
+    let driver = await new Builder().forBrowser('firefox').build();
+    await driver.get('https://ramita-0.github.io/');
+
+    // mouse sobre boton js
+    timeout(500).then(async woke => {
+        const botonJs = driver.findElement({xpath: "/html/body/nav/ul/li[1]/a"})
+        driver.actions().move({duration: 0, origin: botonJs}).perform()
+        console.log("hovered");
+        
+        // entro a la seccion js
+        timeout(650).then(async woke => {
+            botonJs.click()
+
+            // giro el carousel 1 vez
+            timeout(2000).then(async woke => {
+                await driver.findElement({className: "carousel-control-prev-icon"}).click()
+                
+                // mouse sobre una card
+                timeout(700).then(async woke => {
+                    const card0 = driver.findElement({xpath: "/html/body/div[2]/div/div[3]/div/div[1]/div[2]"})
+                    driver.actions().move({duration: 300, origin: card0}).perform()        
+                    
+                    // mouse sobre la otra card
+                    timeout(1500).then(async woke => {
+                        const card1 = driver.findElement({xpath: "/html/body/div[2]/div/div[3]/div/div[2]/div[2]"})
+                        driver.actions().move({duration: 300, origin: card1}).perform()
+                        
+                        // mouse sobre el boton de probar de la card actual
+                        timeout(1000).then(async woke => {
+                            const botonCard1 = driver.findElement({xpath: "/html/body/div[2]/div/div[3]/div/div[2]/div[3]/a"})
+                            driver.actions().move({duration: 0, origin: botonCard1}).perform()
+                        
+                        })
+                    })
+                })
+            })
+        })
+    })
+    
+    
+
+
+    // callbackAfterTimeout(driver, 1, async() => {
+    //     // let el = await driver.findElement({xpath: "/html/body/div[2]/div/div[3]/div/div[2]/div[3]/a"})
+    //     // await driver.wait(until.elementIsVisible(el), 500);
+    //     // await el.click();
+        
+    // })
 })();
